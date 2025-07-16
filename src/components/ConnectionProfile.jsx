90cf5351d76e39717e1f47838f37ea073e8c9616
keshav kumar <keshavks9810@gmail.com>
2025-07-16 23:07:00 +0530

    Error solve and improve UI for connection user profile

--- src/components/ConnectionProfile.jsx
+++ src/components/ConnectionProfile.jsx
@@ -8,17 +8,29 @@ const ConnectionProfile = () => {
     const [user, setUser] = useState("");
     const data  = async()=>{
         const res = await axios(BASE_URL+`/user/connection/${withUserId}`,{withCredentials : true});   
-        setUser(res.data); 
-        console.log(user);               
+        setUser(res?.data?.data); 
+                      
     }
     useEffect(()=>{
         data();
-    },)
+    },[withUserId])
+    
+    console.log(user);
+    
+    if(!user )return <p className="text-center text-2xl">Loading...</p>
 
   return (
     <div className="flex justify-center items-center">
-        <div className="w-[60%]">
-            <p>{user.firstName}</p>
+        <div className="w-[60%] bg-base-100 mt-10">
+            <div className="flex flex-col justify-center items-center">
+                <img src={user.photoUrl} alt="user Profile" className="rounded-full" />
+                <div><p className="text-4xl">{user.firstName +" "+ user.lastName}</p></div>
+            </div>
+            <div>
+            <p>{user.age}</p>
+            <p>{user.gender}</p>
+            <p>{user.about}</p>
+            </div>
         </div>
     </div>
   )
