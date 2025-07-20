49a750b79f8210a4bcde5b11d6875afe505d97a6
keshav kumar <keshavks9810@gmail.com>
2025-07-20 23:29:35 +0530

    Improving Connection Profile

--- src/components/ConnectionProfile.jsx
+++ src/components/ConnectionProfile.jsx
@@ -15,22 +15,32 @@ const ConnectionProfile = () => {
         data();
     },[withUserId])
     
-    console.log(user);
-    
     if(!user )return <p className="text-center text-2xl">Loading...</p>
 
   return (
     <div className="flex justify-center items-center">
-        <div className="w-[60%] bg-base-100 mt-10">
-            <div className="flex flex-col justify-center items-center">
-                <img src={user.photoUrl} alt="user Profile" className="rounded-full" />
-                <div><p className="text-4xl">{user.firstName +" "+ user.lastName}</p></div>
+        <div className="w-[40%] bg-base-100 mt-10 flex flex-row rounded-sm">
+            <div className="w-[50%] p-5">
+                <div className="flex flex-col ">
+                    <img src={user.photoUrl} alt="user Profile" className="rounded-full w-[40%]" />
+                    <div className=""><p className="text-4xl">{user.firstName +" "+ user.lastName}</p></div>
+                    <div className="flex flex-row my-3" >
+                        <div className="flex justify-between">
+                            <p>{user.gender.charAt(0).toUpperCase()+user.gender.slice(1)}</p>
+                            <p>{user.age}</p>
+                        </div>    
+                    </div>
+                </div>
+                <div className=" text-2xl mt-4">
+                    <div className="flex flex-row">
+                    <p>{user.about}</p>
+                    </div>
+                </div>
             </div>
-            <div>
-            <p>{user.age}</p>
-            <p>{user.gender}</p>
-            <p>{user.about}</p>
+            <div className="w-[50%]">
+                {/* <h1>Linkedin</h1> */}
             </div>
+           
         </div>
     </div>
   )
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -35,13 +35,12 @@ const Connection = () => {
         <h1 className="  font-bold text-3xl my-4">Connections</h1>
         {
             connectionData.map((data )=>{
-                const {_id,firstName, lastName, photoUrl, gender, age } = data;
+                const {_id,firstName, lastName, photoUrl} = data;
                 return (
                     <div key={_id} className=' md:w-1/2 h-24 flex items-center bg-white mx-auto justify-between my-4'>
                         <div><img src= {photoUrl} className='w-20 h-20 rounded-full mx-10' alt="UserPhoto"  /></div>
                         <div className='ml-5 text-left'>
-                            <p className='font-bold text-slate-700 text-xl'>{firstName + " "+ lastName}</p>
-                            {(age && gender) ?<p>{age +" "+ gender} </p>: ""}
+                            <Link to={`/connection/${_id}`} className='font-bold text-slate-700 text-xl'>{firstName + " "+ lastName}</Link>
                         </div>
                         <Link to={"/chat/" + _id}><button className='bg-indigo-700 m-2 p-2 rounded-md text-white mx-10'>Chat</button></Link>
                     </div>
